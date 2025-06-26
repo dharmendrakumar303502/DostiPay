6189145f1ccb92f362d65d7244cf92d672eccf4e
dharmendrakumar303502 <dharmendrakumar303502@gmail.com>
2025-06-27 03:40:29 +0530

    Added mock-transactions data file
    Added mock-transactions data file

---
+++ src/data/mock-transactions.ts
@@ -0,0 +1,34 @@
+export const mockTransactions = [
+  {
+    id: 1,
+    date: '2025-06-27',
+    name: 'Zomato',
+    description: 'Food order',
+    amount: 340.0,
+    type: 'Expense',
+  },
+  {
+    id: 2,
+    date: '2025-06-26',
+    name: 'PhonePe',
+    description: 'Received from Ramesh',
+    amount: 800.0,
+    type: 'Income',
+  },
+  {
+    id: 3,
+    date: '2025-06-25',
+    name: 'Amazon',
+    description: 'Online shopping',
+    amount: 1250.0,
+    type: 'Expense',
+  },
+  {
+    id: 4,
+    date: '2025-06-24',
+    name: 'UPI Transfer',
+    description: 'Paid to grocery store',
+    amount: 500.0,
+    type: 'Expense',
+  },
+];
